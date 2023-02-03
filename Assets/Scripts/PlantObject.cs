c5c1b7b579b4410d09a3d28b787b7d6e1528c02f
heikkinenlaura <77092247+heikkinenlaura@users.noreply.github.com>
2023-02-04 01:04:03 +0200

    added store

---
+++ Assets/Scripts/PlantObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Plant", menuName ="Plant")]
+public class PlantObject : ScriptableObject
+{
+    public string plantName;
+    public GameObject[] plantStates;
+    public float timeBtwStates;
+    public int price;
+    public Sprite icon;
+}
